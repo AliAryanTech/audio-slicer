c494931f46c85f035fb4bce607319be53ee3d8b3
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-09-26 12:35:05 +0000

    chore: released 1.1.0

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const exec = promisify(require('child_process').exec);
 const fs = require('fs-extra');
 const path = require('path');
 
-const audioToSlice = async (buffer, seconds) => {
+const audioToSlice = async (buffer, seconds, video = false) => {
   if (!buffer || !seconds)
-    throw new Error('Both audio buffer and seconds are required.');
-
-  const filename = path.join(tmpdir(), `${Math.random().toString(36)}.mp3`);
+    throw new Error('Both media buffer and seconds are required.');
+  
+  const extension = video ? 'mp4' : 'mp3'
+  const options = video ? '-reset_timestamps 1' : '-c:a libmp3lame'
+  const filename = path.join(tmpdir(), `${Math.random().toString(36)}.${extension}`);
   await fs.writeFile(filename, buffer);
   try {
     const directory = 'temporary';
     await fs.ensureDir(directory);
-    await exec(`ffmpeg -i ${filename} -f segment -segment_time ${seconds} -c:a libmp3lame ${directory}/audio_%03d.mp3`);
+    await exec(`ffmpeg -i ${filename} -f segment -segment_time ${seconds} ${options} ${directory}/document_%03d.${extension}`);
     const files = await fs.readdir(directory);
     const buffers = await Promise.all(files.map(x => fs.readFile(path.join(directory, x))));
     await Promise.all([fs.unlink(filename), fs.remove(directory)]);
