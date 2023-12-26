a2424a666ec8582db783c23bd49ab48d6004056a
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-12-26 07:37:49 +0000

    nothing

--- index.js
+++ index.js
@@ -26,4 +26,27 @@ const audioToSlice = async (buffer, seconds, video = false) => {
   }
 }
 
-module.exports = { audioToSlice }
+const audioMerge = async (audios) => {
+    if (audios.length < 2) return audios[0] || [];
+    try {
+        const directory = 'temporary_merge';
+        await fs.ensureDir(directory);
+        audios.forEach(async (buffer, index) => {
+            const filename = path.join(directory, `audio_${index}.mp3`);
+            await fs.writeFile(filename, buffer);
+        });
+        const filename = path.join(tmpdir(), `${Math.random().toString(36)}.mp3`);
+        const files = audios.map((_, index) => `-i ${path.join(directory, `audio_${index}.mp3`)}`).join(' ');
+        await exec(
+            `ffmpeg ${files} -filter_complex concat=n=${audios.length}:v=0:a=1 -strict -2 ${filename}`
+        );
+        const buffer = await fs.readFile(filename);
+        await Promise.all([fs.unlink(filename), fs.remove(directory)]);
+        return buffer;
+    } catch (error) {
+        console.error(error.message);
+        return [];
+    }
+}
+
+module.exports = { audioMerge, audioToSlice }
